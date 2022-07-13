d71a115bbac19709d2ccc4031d76e33acc4c6b42
Gershom Gbadebo <gershomog@gmail.com>
2022-07-13 17:44:40 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
+from flask_behind_proxy import FlaskBehindProxy
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+proxied = FlaskBehindProxy(app)  ## add this line
+
+app.config ['SECRET_KEY'] = 'd3b9f09a91b12d719c792eaeeff942e6'
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) # if so - send to home page
+    return render_template('register.html', title='Register', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
