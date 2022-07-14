2b15d70ec01a14c2550ca52670f2f7afdb9279cb
Gershom Gbadebo <gershomog@gmail.com>
2022-07-14 18:54:15 +0000

    most recent

--- main.py
+++ main.py
@@ -28,5 +28,15 @@ def register():
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
